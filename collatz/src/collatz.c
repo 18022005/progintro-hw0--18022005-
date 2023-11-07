05b07d1c1093792a8d0bcef501444d017a5539e1
18022005 <147803231+18022005@users.noreply.github.com>
2023-11-07 15:16:47 +0200

    Update collatz.c

--- collatz/src/collatz.c
+++ collatz/src/collatz.c
@@ -4,20 +4,20 @@
  int collatz(int n, int max_steps) {
    int steps = 1;
    while(n != 1 ){
-    if(n % 2 ==0){
+    if(n % 2 ==0){      // αν n  αρτιος τοτε n=n/2
       n = n / 2;
     }
     else{
-      n = 3 * n + 1;
+      n = 3 * n + 1;    // αν n περιττος τοτε n=3n+1
     }
     steps++;
 
  }
   return steps;
  }
-
- int main(int argc , char* argv[]){
-   int lowest_number = atoi(argv[1]);
+     
+ int main(int argc , char* argv[]){       //  το argc το χρησιμοποιουμαι για να ελέγξουμε ότι δόθηκε ακριβώς ένα όρισμα στη γραμμή εντολής.
+   int lowest_number = atoi(argv[1]);     //  το argv το χρησσιμοπουμαι με σκοπο να παρουμε τον αριθμό που βαζουμε και το atoi για να γινει ακεραιος
    int highest_number = atoi(argv[2]);
     if(lowest_number > -100000000 && lowest_number < 0 || highest_number > 100000000 ){
         printf("0/n");      
@@ -29,13 +29,13 @@
     for(int i = lowest_number; i<= highest_number; i++)
     {
       int steps = collatz(i, max_steps);
-           if (steps > max_steps) {
+           if (steps > max_steps) {    // αν τα μεγιστα βηματα μικροτερα απο τα βηματα
            max_steps = steps;
            current_number = i;
      }
   }
 
  }
- printf("Ο αριθμός με το μεγαλύτερο αριθμό βημάτων Collatz ανάμεσα στους %d και %d είναι %d με %d βήματα.\n", lowest_number, highest_number, current_number, max_steps);
+ printf("Ο αριθμός με το μεγιστο αριθμό βημάτων μεταξυ %d και %d είναι %d με %d βήματα.\n", lowest_number, highest_number, current_number, max_steps);
  }
 
